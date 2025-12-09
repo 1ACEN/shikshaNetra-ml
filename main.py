a4147a1a58a8689866b7e1c0928aeed7413f4ec1
Vikas Saini <bt24csd047@iiitn.ac.in>
2025-12-09 17:43:11 +0530

    Convert script to FastAPI application

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from fastapi import FastAPI
 from src.pipeline import process_session
 
-if __name__ == "__main__":
-    print("Shiksha Netra ML Pipeline")
+app = FastAPI()
 
+@app.get("/")
+def home():
+    return {"message": "Shiksha Netra ML Pipeline Running"}
+
+@app.post("/process")
+def process(data: dict):
+    return process_session(data)
